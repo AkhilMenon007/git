49825b4ae00987b86f7a0acaaba84d66042a29b9
Akhil Menon <menonakhilmenon@gmail.com>
2019-10-13 14:16:18 +0530

    ic

---
+++ AR GAMES/Assets/Models/PlayerRotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRotate : MonoBehaviour
+{
+    public Transform player;
+    public Transform self;
+    // Start is called before the first frame update
+    void Update()
+    {
+        self.rotation = Quaternion.LookRotation(Vector3.ProjectOnPlane(self.position - player.position, Vector3.up), Vector3.up);
+    }
+
+    // Update is called once per frame
+
+}
---
+++ AR GAMES/Assets/Models/Shooter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    public GameObject shotprefab;
+    public Transform shotSpawn;
+    public void Shoot()
+    {
+        Instantiate(shotprefab, shotSpawn.position, shotSpawn.rotation);
+    }
+}
